24d76b0b794f88b75fc44797267715ec5f9c1ee7
Ayush Hiran <hiranayush14@gmail.com>
2024-05-28 18:17:29 +0530

    database added to cloud mongodb

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1:27017/hotel';
+// const url = 'mongodb://127.0.0.1:27017/hotel';
+const url = 'mongodb+srv://ayushhiran:Ayush123@ayushtestdb.5ctdabu.mongodb.net/';
 
 mongoose.connect(url, {
     useNewUrlParser: true,
